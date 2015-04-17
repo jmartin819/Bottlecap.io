112ad982aefcb7d44969a4f137cfab902b5b4f53
Josh Martin <josh.martin819@gmail.com>
2015-04-17 18:20:29 -0400

    Added a regex check to search allowing for partial text to return results.

--- app/routes/searchRoutes.js
+++ app/routes/searchRoutes.js
@@ -7,7 +7,9 @@ exports.searchByUser = function(req, res){
 
 	console.log(req.params.searchstring);
 	
-	User.find( { username: req.params.searchstring}, function(err, users){
+	//User.find( { username: req.params.searchstring}, function(err, users){
+	//db.users.find(name: new RegExp(search));
+	User.find( { username: new RegExp(req.params.searchstring)}, function(err, users){
 		if(err) res.send(err);
 
 		console.log(users);
@@ -20,7 +22,7 @@ exports.searchByCap = function(req, res){
 
 	console.log(req.params.searchstring);
 	
-	Bottlecap.find( { beername: req.params.searchstring}, function(err, caps){
+	Bottlecap.find( { beername: new RegExp(req.params.searchstring)}, function(err, caps){
 		if(err) res.send(err);
 
 		console.log(caps);
@@ -33,7 +35,7 @@ exports.searchByColor = function(req, res){
 
 	console.log(req.params.searchstring);
 	
-	Bottlecap.find( { avgColor: req.params.searchstring}, function(err, caps){
+	Bottlecap.find( { avgColor: new RegExp(req.params.searchstring)}, function(err, caps){
 		if(err) res.send(err);
 
 		console.log(caps);
